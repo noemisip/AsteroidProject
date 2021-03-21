55d23aea0530fca116f3e31530bcb6ce25757aa8
Enikő Bella Lanczki <enikobellalanczki@edu.bme.hu>
2021-03-21 22:51:48 +0100

    billofmaterials ekezetmentes

--- src/BillOfMaterials.java
+++ src/BillOfMaterials.java
@@ -2,14 +2,14 @@ import java.util.ArrayList;
 
 public class BillOfMaterials {
 	//private Material GateList;
-	//ezt a listát a SetUP() metódusok töltik fel, attól függõen, hogy robot, teleportkapu, vagy bázis építését vizsgáljuk
+	//ezt a listat a SetUP() metodusok toltik fel, attol fuggoen, hogy robot, teleportkapu, vagy bazis epiteset vizsgaljuk
 	private ArrayList<Material> materialList;
 
 	public BillOfMaterials(){
 		materialList= new ArrayList<Material>();
 	}
 	public boolean CheckMaterials(ArrayList<Material> materials) {
-		//Összehasonlítja a paraméterül kapott material listával a saját materialList-jét.
+		//Osszehasonlitja a parameterul kapott material listaval a sajat materialList-jet.
 		Controller cnt = new Controller();
 		cnt.SetTab(1);
 		cnt.PrintFunc("CheckMaterials(ArrayList<Material> materials;) : true/false");
@@ -17,64 +17,64 @@ public class BillOfMaterials {
 		while(materialList.size()!=0 && i<materials.size())
 		{
 			int j=0;
-			//A settler alapanyagait hasonlítja a saját materialListjével
+			//A settler alapanyagait hasonlitja a sajat materialListjevel
 			while(j<materialList.size() && !materials.get(i).IsEquales(materialList.get(j))) j++;
-			//Ha megáll a ciklus, és a j még kisebb, mint a lista mérete, akkor talált egyezést, így törölni kell
-			//a materialListbõl
+			//Ha megall a ciklus, es a j meg kisebb, mint a lista merete, akkor talalt egyezest, igy torolni kell
+			//a materialListbol
 			if(j<materialList.size()) RemoveMaterialFromList(materialList.get(j));
 			i++;
 		}
 		cnt.SetTab(-1);
-		//Ha a BillOfMaterials materialListájából az összes alapanyag kitörlõdött,
-		//akkor a settlernek van elég alapanyaga, így építhet.
+		//Ha a BillOfMaterials materialListajabol az osszes alapanyag kitorldott,
+		//akkor a settlernek van eleg alapanyaga, igy epithet.
 		if(materialList.size()==0) return true;
-		else return false; //Ha nem törlõdött ki az összes, akkor nem építhet
+		else return false; //Ha nem torlodott ki az osszes, akkor nem epithet
 	}
 	
 	public void SetUpRobot() {
-		//Feltölti a materialList-et a robot megépítéséhez szükséges alapanyagokkal
+		//Feltolti a materialList-et a robot megepitesehez szukseges alapanyagokkal
 		Controller cnt = new Controller();
 		cnt.SetTab(1);
 		cnt.PrintFunc("SetupRobot()");
-		//A teszt folyamán a robot építéséhez egy Iron és egy Carbon szükséges
-		//Ezekkel tölti fel a materialListet
+		//A teszt folyaman a robot epitesehez egy Iron és egy Carbon szukseges
+		//Ezekkel tolti fel a materialListet
 		materialList.add(new Iron());
 		materialList.add(new Carbon());
 		cnt.SetTab(-1);
 	}
 	
 	public void SetUpGate() {
-		//Feltölti a materialList-et a teleportkapu megépítéséhez szükséges alapanyagokkal
+		//Feltolti a materialList-et a teleportkapu megepitesehez szukseges alapanyagokkal
 		Controller cnt = new Controller();
 		cnt.SetTab(1);
 		cnt.PrintFunc("SetUpGate()");
 
-		//A teszt folyamán a teleportkapu építéséhez egy Uranium és egy Iron szükséges
-		//Ezekkel tölti fel a materialListet
+		//A teszt folyaman a teleportkapu epitesehez egy Uranium és egy Iron szukseges
+		//Ezekkel tolti fel a materialListet
 		materialList.add(new Uranium());
 		materialList.add(new Iron());
 		cnt.SetTab(-1);
 	}
 	
 	public void SetUpBase() {
-		//Feltölti a materialList-et az ûrbázis megépítéséhez szükséges alapanyagokkal
+		//Feltolti a materialList-et az urbazis megepitesehez szukseges alapanyagokkal
 		Controller cnt = new Controller();
 		cnt.SetTab(1);
 		cnt.PrintFunc("SetUpBase()");
 
-		//A teszt folyamán a teleportkapu építéséhez egy Carbon szükséges
-		//Ezzel tölti fel a materialListet
+		//A teszt folyaman a teleportkapu epitesehez egy Carbon szukseges
+		//Ezzel tolti fel a materialListet
 		materialList.add(new Carbon());
 		cnt.SetTab(-1);
 	}
 	
 	public void RemoveMaterials(Settler s) {
-		//A paraméterül kapott telepes material listájából kitörli a materialList-ben található alapanyagokat
+		//A parameterul kapott telepes material listajabol kitorli a materialList-ben talalhato alapanyagokat
 		Controller cnt = new Controller();
 		cnt.SetTab(1);
 		cnt.PrintFunc("RemoveMaterials(Settler s)");
 
-		//A materialList összes elemét kitörli a settler materialjai közül egyenként
+		//A materialList osszes elemet kitorli a settler materialjai kozul egyenkent
 		for (int m=0;m< materialList.size();m++)
 		{
 			s.RemoveMaterial(materialList.get(m));
@@ -82,7 +82,7 @@ public class BillOfMaterials {
 	}
 	
 	public void RemoveMaterialFromList(Material m) {
-		//Kitörli a materialList-bõl a paraméterként kapott materialt.
+		//Kitorli a materialList-bol a parameterkent kapott materialt.
 		Controller cnt = new Controller();
 		cnt.SetTab(1);
 		cnt.PrintFunc("RemoveMaterialFromList(Material m)");
