3ec19e9b5f6c34b6b5f87936ee5a440bd5113f2b
Dorina <Dorina@DESKTOP-5L0KEEE>
2021-03-21 18:47:04 +0100

    kodolás oke

--- src/Controller.java
+++ src/Controller.java
@@ -1,4 +1,4 @@
-﻿import java.util.Scanner;
+import java.util.Scanner;
 
 public class Controller {
 	static  int tab;
--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,4 @@
-﻿import java.util.ArrayList;
+import java.util.ArrayList;
 
 public class Game {
 
