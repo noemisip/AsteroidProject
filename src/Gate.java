9edc6cb6a71c5ddc6acaa859c8a7e1d2f102ceaf
hajni77 <frecskahajni@gmail.com>
2021-03-21 20:59:46 +0100

    asteroid

--- src/Gate.java
+++ src/Gate.java
@@ -5,33 +5,44 @@ public class Gate implements Transport {    // A teleportkaput reprezent�l� 
 
 	public void SetPair(Gate g) {           //Be�ll�tja, hogy melyik kapup�r a p�rja.
 		Controller cnt = new Controller();
+		cnt.SetTab(1);
 		cnt.PrintFunc("SetPair()");
-	    pair = g; 
+		pair = g;
+		cnt.SetTab(-1);
 	}
-	
-	public Gate GetPair() {                 //Visszaadja az adott kapu p�rj�t 
+
+	public Gate GetPair() {                 //Visszaadja az adott kapu p�rj�t
 		Controller cnt = new Controller();
+		cnt.SetTab(1);
 		cnt.PrintFunc("GetPair() : Gate");
-	    return pair;
+		cnt.SetTab(-1);
+		return pair;
 	}
-	
+
 	public void SetAsteroid(Asteroid a) {   //Be�ll�tja, hogy melyik aszteroid�n �ll a kapu.
 		Controller cnt = new Controller();
+		cnt.SetTab(1);
 		cnt.PrintFunc("SetAsteroid(Asteroid a)");
-	    asteroid = a;
+		asteroid = a;
+		cnt.SetTab(-1);
 	}
-	
+
 	public Asteroid GetAsteroid() {        //Az asteroid param�ter �rt�k�t adja vissza.
 		Controller cnt = new Controller();
+		cnt.SetTab(1);
 		cnt.PrintFunc("GetAsteroid(): Asteroid");
-	    return asteroid;
+		cnt.SetTab(-1);
+		return asteroid;
 	}
-	
+
 	public void Transport(Creature c) {    //k�t aszteroida k�z�tti mozg�s teleportkapu �ltal
 		Controller cnt = new Controller();
+		cnt.SetTab(1);
 		cnt.PrintFunc("Transport(Creature c)");
-	    asteroid.AddCreature(c);
+		asteroid.AddCreature(c);
 		c.SetAsteroid(asteroid);
+		cnt.SetTab(-1);
 
 	}
 }
+
