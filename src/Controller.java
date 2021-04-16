fcd87016badd27d04163707a3b853415fc3937a9
Dorina <Dorina@DESKTOP-5L0KEEE>
2021-04-16 11:06:42 +0200

    controller

--- src/Controller.java
+++ src/Controller.java
@@ -4,195 +4,262 @@ import java.util.Scanner;
 import static java.lang.Integer.parseInt;
 
 public class Controller {
-	private HashMap<String, Object> hash = new HashMap<String, Object>();
+    private HashMap<String, Object> hash = new HashMap<String, Object>();
+    private int settlerCnt =0;
+    private int gateCnt = 0;
+    private int asteroidCnt = 0;
+    private int ufoCnt =0;
 
+    public void ReadFromConsole() {
+        Scanner s = new Scanner(System.in);
+        String[] cmd = {""};
+        while (s.hasNextLine()) {
+            String line = s.nextLine();
+            if (!line.isEmpty()) cmd = line.split(" ");
+        }
+        switch (cmd[0]) {
+            case "place":
+                Place(cmd);
+                break;
+            case "kerge":
+                Kerge_Gate(cmd);
+                break;
+            case "move":
+                Move(cmd);
+                break;
+            case "drill":
+                Drill(cmd);
+                break;
+            case "mine":
+                Mine(cmd);
+                break;
+            case "create":
+                Create(cmd);
+                break;
+            case "restore":
+                Restore(cmd);
+                break;
+            case "settler":
+                Settler_Properties(cmd);
+                break;
+            case "asteroid":
+                Asteroid_Properties(cmd);
+                break;
+            case "open":
+                Open(cmd);
+                break;
+            case "check":
+                Check(cmd);
+                break;
+            case "setup":
+                Setup(cmd);
+                break;
+            case "set":
+                Set(cmd);
+                break;
+            case "help":
+                Help();
+                break;
+            case "SolarStorm":
+                SolarStorm(cmd);
+                break;
+            default:
+                break;
+        }
 
-	public void ReadFromConsole() {
-		Scanner s = new Scanner(System.in);
-		String[] cmd = {""};
-		while(s.hasNextLine()) {
-			String line = s.nextLine();
-			if(!line.isEmpty()) cmd=line.split(",");
-		}
-		switch (cmd[0]){
-			case "place" :
-				Place(cmd);
-				break;
-			case "kerge" :
-				Kerge_Gate(cmd);
-				break;
-			case "move" :
-				Move(cmd);
-				break;
-			case "drill" :
-				Drill(cmd);
-				break;
-			case "mine" :
-				Mine(cmd);
-				break;
-			case "create" :
-				Create(cmd);
-				break;
-			case "restore" :
-				Restore(cmd);
-				break;
-			case "settler" :
-				Settler_Properties(cmd);
-				break;
-			case "asteroid" :
-				Asteroid_Properties(cmd);
-				break;
-			case "open" :
-				Open(cmd);
-				break;
-			case "check" :
-				Check(cmd);
-				break;
-			case "setup" :
-				Setup(cmd);
-				break;
-			case "set" :
-				Set(cmd);
-				break;
-			case "help" :
-				Help();
-				break;
-			case "SolarStorm" :
-				SolarStorm(cmd);
-				break;
-			default : break;
-		}
+    }
 
-	}
-	public void Place(String[] cmd){
-		Settler s = (Settler) hash.get(cmd[1]);
-		Gate g = (Gate) hash.get(cmd[2]);
-		s.PlaceGate(g);
-	}
-	public void Kerge_Gate(String[] cmd){ //TODO
-		Gate g = (Gate) hash.get(cmd[1]);
-	}
-	public void Move(String[] cmd){
-		Creature c = (Creature) hash.get(cmd[1]);
-		c.Move((Asteroid) hash.get(cmd[2]));
-	}
-	public void Drill(String[] cmd){
-		Settler s = (Settler) hash.get(cmd[1]);
-		s.Drill();
-	}
-	public void Mine(String[] cmd){
-		Settler s = (Settler) hash.get(cmd[1]);
-		s.Mine();
-	}
-	public void Create(String[] cmd){
-		if(cmd[1].equals("robot")){
-			Settler s = (Settler) hash.get(cmd[2]);
-			s.CreateRobot();
-		}
-		else if(cmd[1].equals("gate")){
-			Settler s = (Settler) hash.get(cmd[3]);
-			s.CreateGate();
-			hash.put(cmd[1], s.GetGate(0));
-			hash.put(cmd[2], s.GetGate(1));
-		}
-	}
-	public void Restore(String[] cmd){
-		Settler s = (Settler) hash.get(cmd[1]);
-		Material m = null;
-		switch (cmd[2]){
-			case "iron":
-				m = new Iron();
-				break;
-			case "ice":
-				m = new Ice();
-				break;
-			case "carbon":
-				m = new Carbon();
-				break;
-			case "uranium":
-				m = new Uranium();
-				break;
-			default:break;
-		}
-		s.RemoveMaterial(m);
-	}
-	public void Settler_Properties(String[] cmd){
-		Settler s = (Settler) hash.get(cmd[2]);
-	}
-	public void Asteroid_Properties(String[] cmd){
-		Asteroid a = (Asteroid) hash.get(cmd[2]);
-	}
-	public void Open(String[] cmd){
+    public void Place(String[] cmd) {
+        Settler s = (Settler) hash.get(cmd[1]);
+        Gate g = (Gate) hash.get(cmd[2]);
+        s.PlaceGate(g);
+    }
 
-	}
-	public void Check(String[] cmd){
-		if(cmd[1].equals("settlers")){
-			Game.getInstance().CheckSettlers();
-		}
-		else if(cmd[1].equals("base")){
-			Asteroid a = (Asteroid) hash.get(cmd[2]);
-			Game.getInstance().CheckBase(a);
-		}
-	}
-	public void Setup(String[] cmd){
-		int k=0;
-		if(cmd.length==1){
-			Space sp = new Space();
-			Game.getInstance().SetSpace(sp);
-			return;
-		}
-		else if(cmd[2].equals("r")){
+    public void Kerge_Gate(String[] cmd) { //TODO
+        Gate g = (Gate) hash.get(cmd[1]);
+    }
 
-		}
-		else{
-			k = parseInt(cmd[2]);
-		}
-		if(cmd[1].equals("asteroid")){
-			for(int i=0; i<k; i++){
-				Asteroid a = new Asteroid();
-				hash.put("a", a);
-			}
-		}
-		else if(cmd[1].equals("settler")){
-			for(int i=0; i<k; i++){
-				Settler s = new Settler();
-				hash.put("s", s);
-			}
-		}
-		else if(cmd[1].equals("ufo")){
-			for(int i=0; i<k; i++){
-				Ufo u = new Ufo();
-				hash.put("u", u);
-			}
-		}
-		else if(cmd[1].equals("gate")){
-			for(int i=0; i<k; i++){
-				Gate g1 = new Gate();
-				Gate g2 = new Gate();
-				g1.SetPair(g2);
-				g2.SetPair(g1);
-				hash.put("g", g1);
-				hash.put("g", g2);
-			}
-		}
-	}
-	public void Set(String[] cmd){
-		if(cmd[1].contains("asteroid")){
-			Asteroid a = (Asteroid) hash.get(cmd[1]);
-			a.SetMaterial();
-			a.SetLayer(parseInt(cmd[3]));
-			a.SetCloseToSun(cmd[4].equals("true"));
-		}
-		else if(cmd[1].contains("settler")){}
-		else if(cmd[1].contains("ufo")){}
-		else if(cmd[1].contains("uranium")){}
-	}
-	public void Help(){
+    public void Move(String[] cmd) {
+        Creature c = (Creature) hash.get(cmd[1]);
+        c.Move((Asteroid) hash.get(cmd[2]));
+    }
 
-	}
-	public void SolarStorm(String[] cmd){
-		Asteroid a = (Asteroid)hash.get(cmd[1]);
-		a.SolarStorm();
-	}
+    public void Drill(String[] cmd) {
+        Settler s = (Settler) hash.get(cmd[1]);
+        s.Drill();
+    }
+
+    public void Mine(String[] cmd) {
+        Settler s = (Settler) hash.get(cmd[1]);
+        s.Mine();
+    }
+
+    public void Create(String[] cmd) {
+        if (cmd[1].equals("robot")) {
+            Settler s = (Settler) hash.get(cmd[2]);
+            s.CreateRobot();
+        } else if (cmd[1].equals("gate")) {
+            Settler s = (Settler) hash.get(cmd[3]);
+            s.CreateGate();
+            hash.put(cmd[1], s.GetGate(0));
+            hash.put(cmd[2], s.GetGate(1));
+        }
+    }
+
+    public void Restore(String[] cmd) {
+        Settler s = (Settler) hash.get(cmd[1]);
+        Material m = null;
+        switch (cmd[2]) {
+            case "iron":
+                m = new Iron();
+                break;
+            case "ice":
+                m = new Ice();
+                break;
+            case "carbon":
+                m = new Carbon();
+                break;
+            case "uranium":
+                m = new Uranium();
+                break;
+            default:
+                break;
+        }
+        s.RemoveMaterial(m);
+    }
+
+    public void Settler_Properties(String[] cmd) {
+        Settler s = (Settler) hash.get(cmd[2]);
+    }
+
+    public void Asteroid_Properties(String[] cmd) {
+        Asteroid a = (Asteroid) hash.get(cmd[2]);
+    }
+
+    public void Open(String[] cmd) {
+
+    }
+
+    public void Check(String[] cmd) {
+        if (cmd[1].equals("settlers")) {
+            Game.getInstance().CheckSettlers();
+        } else if (cmd[1].equals("base")) {
+            Asteroid a = (Asteroid) hash.get(cmd[2]);
+            Game.getInstance().CheckBase(a);
+        }
+    }
+
+    public void Setup(String[] cmd) {
+        int k = 0;
+        if (cmd.length == 1) {
+            Space sp = new Space();
+            Game.getInstance().SetSpace(sp);
+            return;
+        } else if (cmd[2].equals("r")) {
+
+        } else {
+            k = parseInt(cmd[2]);
+        }
+        if (cmd[1].equals("asteroid")) {
+            for (int i = 0; i < k; i++) {
+                Asteroid a = new Asteroid();
+                hash.put("a"+asteroidCnt, a);
+                asteroidCnt++;
+            }
+        } else if (cmd[1].equals("settler")) {
+            for (int i = 0; i < k; i++) {
+                Settler s = new Settler();
+                hash.put("s"+settlerCnt, s);
+                settlerCnt++;
+            }
+        } else if (cmd[1].equals("ufo")) {
+            for (int i = 0; i < k; i++) {
+                Ufo u = new Ufo();
+                hash.put("u"+ufoCnt, u);
+                ufoCnt++;
+            }
+        } else if (cmd[1].equals("gate")) {
+            for (int i = 0; i < k; i++) {
+                Gate g1 = new Gate();
+                Gate g2 = new Gate();
+                g1.SetPair(g2);
+                g2.SetPair(g1);
+                hash.put("g"+gateCnt, g1);
+                gateCnt++;
+                hash.put("g"+gateCnt, g2);
+                gateCnt++;
+            }
+        }
+    }
+
+    public void Set(String[] cmd) {
+        if (cmd[1].contains("asteroid")) {
+            Asteroid a = (Asteroid) hash.get(cmd[1]);
+            Material m = null;
+            switch (cmd[2]) {
+                case "iron":
+                    m = new Iron();
+                    break;
+                case "ice":
+                    m = new Ice();
+                    break;
+                case "carbon":
+                    m = new Carbon();
+                    break;
+                case "uranium":
+                    m = new Uranium();
+                    break;
+                default:
+                    break;
+            }
+            a.SetMaterial(m);
+            a.SetLayer(parseInt(cmd[3]));
+            a.SetCloseToSun(cmd[4].equals("true"));
+            if (!cmd[5].equals("-")) {
+                String[] creatures = cmd[5].split(";");
+                for (String s : creatures) {
+                    a.AddCreature((Creature) hash.get(s));
+                }
+            }
+            if (!cmd[6].equals("-")) {
+                String[] neighbours = cmd[6].split(";");
+                for (String s : neighbours) {
+                    a.AddCreature((Creature) hash.get(s));
+                }
+            }
+        } else if (cmd[1].contains("settler")) {
+            Settler s = (Settler) hash.get(cmd[1]);
+            s.SetAsteroid((Asteroid) hash.get(cmd[2]));
+            if (!cmd[3].equals("-")) {
+                String[] materials = cmd[3].split(";");
+                for (String str : materials) {
+                    s.AddMaterial((Material) hash.get(str));
+                }
+            }
+            if (!cmd[4].equals("-")) {
+                String[] gates = cmd[4].split(";");
+                for (String str : gates) {
+                    s.AddGate((Gate) hash.get(str));
+                }
+            }
+        }
+        else if (cmd[1].contains("ufo")) {
+            Ufo u = (Ufo) hash.get(cmd[1]);
+            u.SetAsteroid((Asteroid) hash.get(cmd[2]));
+        }
+        else if (cmd[1].contains("uranium")) {
+            Uranium u = (Uranium)hash.get(cmd[1]);
+            Asteroid a = (Asteroid) hash.get(cmd[2]);
+            a.SetMaterial(u);
+            u.SetCnt(parseInt(cmd[3]));
+        }
+    }
+    public void Help() {
+
+    }
+    public void SolarStorm (String[]cmd){
+        Asteroid a = (Asteroid) hash.get(cmd[1]);
+        a.SolarStorm(1);
+    }
 
 }
--- src/Robot.java
+++ src/Robot.java
@@ -13,7 +13,7 @@ public class Robot extends Creature implements Steppable {
 	}
 
 	public void Drill(){
-		if ( asteroid.getLayer() != 0){
+		if ( asteroid.GetLayer() != 0){
 			asteroid.DecreaseLayer();
 		}
 	}
@@ -29,53 +29,28 @@ public class Robot extends Creature implements Steppable {
 	}
 
 	public void Step() { //a robot vegrehajtja a kivalasztott muveletet
-		Controller c = new Controller();
-		c.SetTab(1);
-		c.PrintFunc("Step()");
-
 		int result = NextStep();
 		if(result == 0) Drill();
 		else if(result==1) WhereToMove();
-		c.SetTab(-1);
 	}
 	
 	public void Die() { // a robot meghal
-		Controller c = new Controller();
-		c.SetTab(1);
-		c.PrintFunc("Die()");
-
 		asteroid.RemoveCreature(this); //a robot kitorlodik az aszterodia creture listajabol
 		asteroid.GetSpace().RemoveCreature(this); //a robot megkerdezi az aszteroidat, hogy melyik spaceben van és kitorlodik az space creture listajabol
 		Game.getInstance().RemoveSteppable(this); //a robot kitorlodik az space steppable listajabol
-
-		c.SetTab(-1);
 	}
 	
 	public void Move(Asteroid a) { //a robot a kiavlasztott aszteroidara mozog
-		Controller c = new Controller();
-		c.SetTab(1);
-		c.PrintFunc("Move(a)");
-
 		asteroid.GetNeighbours(); //a robot megkapja, hogy melyik transport objektum fogja atvinni a masik aszteroidára
 		a.Transport(this); //meghivja az objektum transport fuggvenyet
 		asteroid.RemoveCreature(this); //a robot kitorlodik az aszterodia creture listajabol
-		c.SetTab(-1);
 	}
 	
 	public void AsteroidExplosion() { //a robot reagal az aszteroida felrobbanasara
-		Controller c = new Controller();
-		c.SetTab(1);
-		c.PrintFunc("AsteroidExplosion()");
-
 		WhereToMove();
-		c.SetTab(-1);
 	}
 	
 	public ArrayList<Material> GetMaterials() { //a robot megmondja, hogy milyen nyersanyagok vannak nala
-		Controller c = new Controller();
-		c.SetTab(1);
-		c.PrintFunc("GetMaterials() : null");
-		c.SetTab(-1);
 		return null; // a robotnál nincs nyersanyag
 	}
 }
