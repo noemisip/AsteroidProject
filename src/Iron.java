3ee4f89ee527f8d2cfae343ffbaf0001235962f3
zakarias00 <79679983+zakarias00@users.noreply.github.com>
2021-04-16 12:19:36 +0300

    iron jo

--- src/Iron.java
+++ src/Iron.java
@@ -1,10 +1,10 @@
 
 public class Iron extends Material {
 	// Akkor hivodik meg ha asnak az aszteroidan
-	public void ReactToMine(Asteroid a, Settler s) {
+	public void ReactToMine(Asteroid a, Ceature c) {
 		Controller c = new Controller();
 		a.SetMaterial(null); // kiszedi az aszteroidabol az eddig bennelevo anyagot, igy ures lesz
-		s.AddMaterial(this); //hozzaadja a kibanyaszott anyagot a settler materialjaihoz
+		c.AddMaterial(this); //hozzaadja a kibanyaszott anyagot a settler materialjaihoz
 	}
 	
 	public void ReactToSunclose(Asteroid a) {
