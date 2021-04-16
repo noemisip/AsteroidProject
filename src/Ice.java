5d10531a76416f9eb8ae19c2916cd4b166134e3d
Dorina <Dorina@DESKTOP-5L0KEEE>
2021-04-16 11:35:00 +0200

    material javitott

--- src/Ice.java
+++ src/Ice.java
@@ -1,20 +1,17 @@
 public class Ice extends Material {
 	// Akkor hivodik meg ha asnak az aszteroidan
 	public void ReactToMine(Asteroid a, Creature c) {
-		Controller c = new Controller();
 		a.SetMaterial(null); // kiszedi az aszteroidabol az eddig bennelevo anyagot, igy ures lesz
 		c.AddMaterial(this); //hozzaadja a kibanyaszott anyagot a settler materialjaihoz
 	}
 	
 	public void ReactToSunclose(Asteroid a) {
 		// Akkor hivodik meg, ha az aszteroida napkozelben van
-		Controller c = new Controller();
 		a.SetMaterial(null); // a jeg eltunik az aszteroidabol, ures lesz
 	}
 	
 	public boolean IsEquales(Material m) {
 		// ellenorzi, hogy a megadott material ugyanolyan fajta- e, mint az osztaly
-		Controller c = new Controller();
 		return m.getClass()==this.getClass();
 	}
 	
--- src/Iron.java
+++ src/Iron.java
@@ -1,20 +1,18 @@
 
 public class Iron extends Material {
 	// Akkor hivodik meg ha asnak az aszteroidan
-	public void ReactToMine(Asteroid a, Ceature c) {
-		Controller c = new Controller();
+	public void ReactToMine(Asteroid a, Creature c) {
 		a.SetMaterial(null); // kiszedi az aszteroidabol az eddig bennelevo anyagot, igy ures lesz
 		c.AddMaterial(this); //hozzaadja a kibanyaszott anyagot a settler materialjaihoz
 	}
 	
 	public void ReactToSunclose(Asteroid a) {
 		// Akkor hivodik meg, ha az aszteroida napkozelben van
-		Controller c = new Controller();
 	}
-	
+
+
 	public boolean IsEquales(Material m) {
 		// ellenorzi, hogy a megadott material ugyanolyan fajta- e, mint az osztaly
-		Controller c = new Controller();
 		return m.getClass()==this.getClass();
 	}
 	
