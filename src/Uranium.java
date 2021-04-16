e983bc32fd8a7e949179237b2818ec101e89b016
Dorina <Dorina@DESKTOP-5L0KEEE>
2021-04-16 11:33:33 +0200

    uranium javitott

--- src/Uranium.java
+++ src/Uranium.java
@@ -1,36 +1,33 @@
 
 public class Uranium  extends Material {
-	
+
 	private int cnt = 0;
-	
+
 	// Akkor hivodik meg ha asnak az aszteroidan
 	public void ReactToMine(Asteroid a, Creature c) {
-		Controller c = new Controller();
 		a.SetMaterial(null); // kiszedi az aszteroidabol az eddig bennelevo anyagot, igy ures lesz
 		c.AddMaterial(this); //hozzaadja a kibanyaszott anyagot a settler materialjaihoz
 	}
-	
+
 	public void ReactToSunclose(Asteroid a) {
 		// Akkor hivodik meg, ha az aszteroida napkozelben van
-		Controller c = new Controller();
-		if(a.getMaterial().getCnt() < 2)
-			a.getMaterial().setCnt(1);
-		if(a.getMaterail().getCnt() == 2)
+		if(cnt < 2)
+			SetCnt(1);
+		if(cnt == 2)
 			a.Explosion();
 	}
-	
+
 	public boolean IsEquales(Material m) {
 		// ellenorzi, hogy a megadott material ugyanolyan fajta- e, mint az osztaly
-		Controller c = new Controller();
 		return m.getClass()==this.getClass();
 	}
-	
+
 	void SetCnt(int cnt) {
 		this.cnt -= cnt;
 	}
-	
+
 	int GetCnt() {
-		return this.cnt;
+		return cnt;
 	}
-	
+
 }
