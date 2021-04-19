12025fc40470e0bae17cd18f275517ad5ffd3ee0
noemisip <78826479+noemisip@users.noreply.github.com>
2021-04-19 10:51:33 +0200

    Update Robot.java

--- src/Robot.java
+++ src/Robot.java
@@ -5,25 +5,25 @@ import java.util.Random;
 public class Robot extends Creature implements Steppable,AI {
 
 	public void WhereToMove() { //A robot eldonti, hogy melyik aszteroidara mozogjon
-		ArrayList<Transport> a = asteroid.GetNeighbours();
+		ArrayList<Transport> a = asteroid.GetNeighbours();//lekerdezi annak az aszteroidanak a szomszedait, amin epp all
 		if(a.size()==0) Die(); //ha nem tud hova menni (azaz az aszteroidanak, mar nincs szomszedja), akkor meghal
 		else {
 			Random r = new Random();
-			int chosen = r.nextInt(a.size());//lekerdezi annak az aszteroidanak a szomszedait, amin epp all
-			Move(a.get(chosen).GetAsteroid()); //a robot mozog
+			int chosen = r.nextInt(a.size());//kivalaszt egy random aszteroidat a szomszedai kozul
+			Move(a.get(chosen).GetAsteroid()); //a robot mozog erre az aszteroidara
 		}
 	}
 
-	public void Drill(){
-		if ( asteroid.GetLayer() != 0){
+	public void Drill(){ // a robot fur
+		if ( asteroid.GetLayer() != 0){ // ha a layer nem 0, akkor csokkenti 1-el
 			asteroid.DecreaseLayer();
 		}
 	}
 
-	public void AddMaterial(Material m){
+	public void AddMaterial(Material m){ // nem tortenik semmi
 	}
 
-	public int NextStep() {//a robot eldonti, hogy a kovetkezo lepesben mit csinaljon
+	public int NextStep() {//a robot eldonti, hogy a kovetkezo lepesben mit csinaljon, 0-t vagy 1-et ad vissza a fuggveny
 		Random r = new Random();
 		int random = r.nextInt(2);
 		return random;
@@ -32,8 +32,8 @@ public class Robot extends Creature implements Steppable,AI {
 
 	public void Step() { //a robot vegrehajtja a kivalasztott muveletet
 		int result = NextStep();
-		if(result == 0) Drill();
-		else if(result==1) WhereToMove();
+		if(result == 0) Drill(); // 0 eseten fur
+		else if(result==1) WhereToMove(); //1 eseten mozog
 	}
 	
 	public void Die() { // a robot meghal
@@ -44,11 +44,11 @@ public class Robot extends Creature implements Steppable,AI {
 	
 	public void Move(Asteroid a) { //a robot a kiavlasztott aszteroidara mozog
 		Asteroid al = asteroid;
-		ArrayList<Transport> neighbours = asteroid.GetNeighbours(); //a telepes ellenorzi, hogy at tud-e menni arra az aszteroidara
+		ArrayList<Transport> neighbours = asteroid.GetNeighbours();  //a szomszed aszteroidak lekerese
 		boolean move = false;
 		Transport tr = null;
 		for(Transport t : neighbours){
-			if(t.GetAsteroid() == a) {
+			if(t.GetAsteroid() == a) { //ha megtalalja a szomszedok kozott a kapott aszteroidat akkor mozogni fog
 				move = true;
 				tr=t;
 				break;
