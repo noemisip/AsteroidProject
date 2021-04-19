d3ed5d1d9b61cebeee099586d8b10681331e2aef
noemisip <78826479+noemisip@users.noreply.github.com>
2021-04-19 11:15:11 +0200

    Update AI.java

--- src/AI.java
+++ src/AI.java
@@ -1,5 +1,4 @@
-public interface AI {
-
-    public abstract int NextStep();
-    public abstract void WhereToMove();
+public interface AI { // interface amit megvalosit az ufo, a gate es a robot
+    public abstract int NextStep(); //random 0-t vagy 1-et ad vissza
+    public abstract void WhereToMove(); //kivalasztja hova mozogjon az objektum
 }
