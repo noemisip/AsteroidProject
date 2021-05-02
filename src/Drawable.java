160d8c41bd65b8de36bc5f46eb88cf078efc77c4
Sipos Noémi <siposnoi0731@gmail.com>
2021-05-02 16:38:03 +0200

    Drawable

--- src/Drawable.java
+++ src/Drawable.java
@@ -1,6 +1,7 @@
 public abstract class Drawable {
     private double x;
     private double y;
+    private View view;
 
     public abstract void Draw();
     public abstract void Update();
@@ -9,7 +10,7 @@ public abstract class Drawable {
         this.y = y;
     }
     public void Remove(){
-
+        //eltunteti a drawable peldanyt a kepernyorol
     }
     public abstract String GetName();
     public double GetX(){
