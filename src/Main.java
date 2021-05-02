b01e27bb2f5c34a16e28e313347ac3ee47671b50
Dorina <Dorina@DESKTOP-5L0KEEE>
2021-05-02 16:11:36 +0200

    tiszta lap :)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 
 public class Main {
-// A tesztprogram inditasa
+
 	public static void main(String[] args) {
-		Controller cnt = new Controller();
-		//cnt.ReadFromConsole();
-		cnt.Menu();
 
 	}
 
