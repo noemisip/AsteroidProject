19d28782e111ede2dc6eb325e670d3dab97c09de
Enikő Bella Lanczki <enikobellalanczki@edu.bme.hu>
2021-05-02 17:50:14 +0200

    ControlPanelnew

--- src/ControlPanel.java
+++ src/ControlPanel.java
@@ -1,10 +1,67 @@
+import javax.swing.*;
 import java.util.ArrayList;
 
 public class ControlPanel {
     private Settler s;
     private ArrayList<String> text=new ArrayList<String>();
+    private JPanel jp;
+    private Asteroid asteroid;
+    private Material material;
+    private Gate gate;
+    JButton move;
+    JButton drill;
+    JButton mine;
+    JButton restore;
+    JButton create_robot;
+    JButton create_gate;
+    JButton place_gate;
+    JLabel settler_label;
+    JLabel asteroid_label;
+    JLabel material_label;
+    JLabel gate_label;
 
+    public void SetText(){
+        text.add("Asteroid: "+Main.getInstance().GetKey(s.GetAsteroid()));
+        String gates="";
+        for(int i=0;i<s.GetGateList().size();i++){
+            gates=gates+Main.getInstance().GetKey(s.GetGateList().get(i))+",";
+        }
+        gates.substring(0, gates.length() - 1);
+        text.add("Gates: "+gates);
+        String materials="";
+        for(int i=0;i<s.GetMaterials().size();i++){
+            materials=materials+Main.getInstance().GetKey(s.GetMaterials().get(i))+",";
+        }
+        materials.substring(0, materials.length() - 1);
+        text.add("Materials: "+materials);
+
+    }
+    public void Init(){
+        jp=new JPanel();
+        move=new JButton("Move");
+        drill=new JButton("Drill");
+        mine=new JButton("Mine");
+        restore=new JButton("Restore");
+        create_robot=new JButton("Create Robot");
+        create_gate=new JButton("Create Gate");
+        place_gate=new JButton("Place Gate");
+        jp.add(move);jp.add(drill);jp.add(mine);jp.add(restore);jp.add(create_robot);
+        jp.add(create_gate); jp.add(place_gate);
+        settler_label=new JLabel(Main.getInstance().GetKey(s));
+        asteroid_label=new JLabel(text.get(0));
+        material_label=new JLabel(text.get(1));
+        gate_label=new JLabel(text.get(2));
+        jp.add(settler_label);jp.add(asteroid_label);jp.add(material_label);jp.add(gate_label);
+    }
     public void SetSettler(Settler settler){s=settler;}
-    public void Update(){}
+    public void Update(){
+        SetText();
+        settler_label.setText(text.get(0));
+        material_label.setText(text.get(1));
+        gate_label.setText(text.get(2));
+    }
+    public Gate GetGate(){return gate;}
+    public Asteroid GetAsteroid(){return asteroid;}
+    public Material GetMaterial(){return material;}
     public int UserInput(){return 1;}
 }
