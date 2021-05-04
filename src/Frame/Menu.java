30c69e78531f681fff44a666193a44c80bc1895b
Dorina <Dorina@DESKTOP-5L0KEEE>
2021-05-04 19:16:26 +0200

    menus

--- src/Frame/Menu.java
+++ src/Frame/Menu.java
@@ -13,27 +13,10 @@ public class Menu extends JFrame {
     private final JButton exit = new JButton("Exit");
 
     public Menu(){
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.setResizable(false);
-//        frame.setLayout(new BorderLayout());
-//        frame.setSize(2000,1000);
-//        frame.setVisible(true);
-//
-//       JLabel contentPane = new JLabel(){
-//        public void paintComponent(Graphics g){
-//            Image img = Toolkit.getDefaultToolkit().getImage(Menu.class.getResource("menu.png"));
-//            g.drawImage(img, 0,0,this.getWidth(), this.getHeight(), this);
-//        }
-//       };
-//       contentPane.setBorder(new EmptyBorder(5,5,5,5));
-//       contentPane.setLayout(new BorderLayout(0,0));
-//       frame.setContentPane(contentPane);
-
-        super("window");
-        this.setLocationRelativeTo(null);
+        super("Aszteroida bányászat");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         try{
-            setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("C:\\MenuSima.jpg")))));
+            setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("menu.png")))));
 
         }catch(IOException e)
         {
