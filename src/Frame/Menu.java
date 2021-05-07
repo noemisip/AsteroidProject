e6625eeb76c5fe2fa54592c765511c576db59e65
Dorina <Dorina@DESKTOP-5L0KEEE>
2021-05-07 11:20:17 +0200

    menus

--- src/Frame/Menu.java
+++ src/Frame/Menu.java
@@ -7,21 +7,22 @@ import java.io.File;
 import java.io.IOException;
 
 public class Menu extends JFrame{
-    private JFrame frame = new JFrame("Aszteroida bányászat");
+    private JFrame frame = new JFrame("Aszteroida banyaszat");
     private final JButton start = new JButton("START");
     private final JButton exit = new JButton("Exit");
+    JLabel image;
 
-    public Menu(){
+
+    public Menu() throws IOException {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        try{
-            frame.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("menu.png")))));
-
-        }catch(IOException e)
-        {
-            e.printStackTrace();
-
-        }
         frame.setLayout(new BorderLayout());
+        image = new JLabel(new ImageIcon(ImageIO.read(new File("menu.png"))));
+        image.setBounds(0,0,1000, 666);
+        frame.add(image);
+        start.setBounds(440, 280, 120, 40);
+        image.add(start);
+        exit.setBounds(440, 350, 120, 40);
+        image.add(exit);
         frame.setResizable(false);
         frame.pack();
         frame.setVisible(true);
