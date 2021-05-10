bee558ccc160aab636b1829165d2b5ba50ac2d7b
hajni77 <78812524+hajni77@users.noreply.github.com>
2021-05-10 10:03:21 +0200

    Update SettlerNumber.java

--- src/Frame/SettlerNumber.java
+++ src/Frame/SettlerNumber.java
@@ -17,15 +17,15 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
-public class SettlerNumber extends JFrame {
+public class SettlerNumber extends JFrame {  //azt a nezetet valositja meg,ahol a jatek bekeri a felhasznalotol,hogy hany telepessel szeretne jatszani
 
-    private int settlerNr;
-    private JButton okButton;
+    private int settlerNr;        //telepesek szama
+    private JButton okButton;     //dontes veglegesito ok gomb
 
     public SettlerNumber() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         try {
-            this.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("background.png")))));
+            this.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("background.png")))));        //jatek hatterenek beallitasa
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -37,28 +37,28 @@ public class SettlerNumber extends JFrame {
         this.setVisible(true);
         this.setLayout(null);
 
-        JLabel text = new JLabel("How many settlers will play?");
-        text.setBounds(200, 230, 800, 60);
+        JLabel text = new JLabel("How many settlers will play?");     //bekerjuk a telepesek szamat
+        text.setBounds(200, 230, 800, 60);                            //meretek beallitasa
         text.setFont(new Font("Dialog", Font.PLAIN, 50));
         text.setForeground(Color.white);
         this.add(text);
 
-        final JTextField textField = new JTextField();
+        final JTextField textField = new JTextField();              //ide irja a felhasznalo a valaszat
         textField.setBounds(400, 350, 200, 50);
         textField.setColumns(10);
         this.add(textField);
 
-        okButton = new JButton("Ok");
-        okButton.setBounds(450, 500, 100, 50);
+        okButton = new JButton("Ok");                              //veglegesites
+        okButton.setBounds(450, 500, 100, 50);                      //meretek beallitasa
         okButton.setFont(new Font("Dialog", Font.PLAIN, 30));
-        this.add(okButton);
+        this.add(okButton);                                        //gomb hozzaadasa
         okButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
                     settlerNr = Integer.parseInt(textField.getText());
                     dispose();
-                    Main.Load(settlerNr);
+                    Main.Load(settlerNr);                                  //betolti a jatekot a megadott telepesek szamaval
                 } catch (NumberFormatException | IOException nex) {
                     nex.printStackTrace();
                 }
