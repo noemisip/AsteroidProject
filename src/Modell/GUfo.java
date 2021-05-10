43b56831e4cd833403f64602d207bec61289a293
LanczkiB <79630831+LanczkiB@users.noreply.github.com>
2021-05-10 10:22:28 +0200

    Update GUfo.java

--- src/Modell/GUfo.java
+++ src/Modell/GUfo.java
@@ -12,29 +12,29 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class GUfo extends Drawable{
-    private Ufo ufo;
-
+    private Ufo ufo; //az az ufo a jatekban, aminek a mejeleniteset vegzi
+    //ufo kirajzolasa
     public void Draw(Graphics g, ImageObserver i) throws IOException {
-        final BufferedImage dnImg = ImageIO.read(new File("ufo_icon.png"));
-        g.drawImage(dnImg, this.GetX(), this.GetY(),30,30, i);
+        final BufferedImage dnImg = ImageIO.read(new File("ufo_icon.png")); //ufo kepenek beallitasa
+        g.drawImage(dnImg, this.GetX(), this.GetY(),30,30, i); //ufo kepenek kirajzolasa a megfelelo pozicioban es meretben
     }
-    public void Update(){
-        Asteroid asteroid=ufo.GetAsteroid();
+    public void Update(){ //pozicio frissitese
+        Asteroid asteroid=ufo.GetAsteroid(); //jelenlegi aszteroida beallitasa
         ArrayList<Steppable> sp=Game.getInstance().GetSteppables();
         int index= sp.indexOf(ufo);
-        if(index!=-1){
-            view.FindAsteroid(asteroid,this);
-            view.GetGameFrame().repaint();
-            view.GetGameFrame().GetFieldPanel().repaint();
-        }else{
+        if(index!=-1){ //Ha meg jatekban van az ufo
+            view.FindAsteroid(asteroid,this); //jelenlegi aszteroida szerinti koordinatak beallitasa
+            view.GetGameFrame().repaint(); //GameFrame frissitese
+            view.GetGameFrame().GetFieldPanel().repaint(); //FieldPanel frissitese
+        }else{ //Ha nincs jatekban az ufo, akkor nem rajzolja ki
             Remove();
         }
     }
-    public String GetName(){
+    public String GetName(){ //visszaadja a nevet
         return null;
     }
 
-    public void SetUfo(Ufo u){
+    public void SetUfo(Ufo u){ //ufo bealltasa
         ufo=u;
     }
 }
