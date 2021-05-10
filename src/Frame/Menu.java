e85f9cd1092f8e11c7ac592d5a6ac9868ce55e59
hajni77 <78812524+hajni77@users.noreply.github.com>
2021-05-10 10:08:35 +0200

    Update Menu.java

--- src/Frame/Menu.java
+++ src/Frame/Menu.java
@@ -8,29 +8,29 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 
-public class Menu extends JFrame{
-    private final JButton start = new JButton("START");
-    private final JButton exit = new JButton("Exit");
+public class Menu extends JFrame{                                   //az elso nezet ami megjelenik a felhasznalo elott, itt tudja elinditani a jatekot, vagy kilepni
+    private final JButton start = new JButton("START");             //jatek inditasa
+    private final JButton exit = new JButton("Exit");               //kilepes a jatekbol
     JLabel image;
 
 
-    public Menu() throws IOException {
+    public Menu() throws IOException {                                     //megjelenitjuk az ablakot egy keretben
         super("Aszteroida banyaszat");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
-        image = new JLabel(new ImageIcon(ImageIO.read(new File("menu.png"))));
-        image.setBounds(0,0,1000, 666);
+        image = new JLabel(new ImageIcon(ImageIO.read(new File("menu.png"))));    //hatter beallitasa
+        image.setBounds(0,0,1000, 666);                                            //meretek megadasa
         this.add(image);
         start.setBounds(440, 280, 120, 40);
         image.add(start);
-        start.addActionListener(new ActionListener() {
+        start.addActionListener(new ActionListener() {                          //esemeny start gomb nyomasa eseten
             @Override
             public void actionPerformed(ActionEvent e) {
                 SettlerNumber sn = new SettlerNumber();
                 dispose();
             }
         });
-        exit.setBounds(440, 350, 120, 40);
+        exit.setBounds(440, 350, 120, 40);                                      //esemeny exit gomb nyomasa eseten
         image.add(exit);
         exit.addActionListener(new ActionListener() {
             @Override
