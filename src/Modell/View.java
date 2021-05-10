64ffdcec1f883965b253fca9c349f544aa76b30a
zakarias00 <79679983+zakarias00@users.noreply.github.com>
2021-05-10 11:29:21 +0300

    Update View.java
    komment

--- src/Modell/View.java
+++ src/Modell/View.java
@@ -8,16 +8,20 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
+//a felhasznaloi nezetert felelos osztaly
 public class View {
 
-	private ArrayList<Drawable> drawables = new ArrayList<Drawable>();
-	private SettlerNumber settlernumberFrame;
-	private Menu menuFrame;
-	private GameFrame gameFrame;
+	private ArrayList<Drawable> drawables = new ArrayList<Drawable>(); //a megjelenitett dolgok listaja
+	private SettlerNumber settlernumberFrame; //a jatekosok szamat bekero ablakhoz tartozo attributum
+	private Menu menuFrame; //menu ablakhoz tartozo attributum
+	private GameFrame gameFrame; //a jatekot megjelenito ablakhoz tartozo attributum
 
+	//a konstruktor meghivja a Menu osztaly kostruktorat
 	public View() throws IOException {
 		Menu();
 	}
+	
+	//vegigmegy a drawables listan es meghivja az Update fuggvenyuket, illetve a gameFrame controlPanel-jenek az Update fuggvenyet is
 	public void UpdateAll() {
 		for(int i=0; i<drawables.size();i++ ){
 			drawables.get(i).Update();
@@ -25,14 +29,18 @@ public class View {
 		gameFrame.GetControlPanel().Update();
 	}
 
+	//a drawables lista getter fuggvenye
 	public ArrayList<Drawable> GetDrawables() {
 		return drawables;
 	}
 
+	//ha napvihar van, megjeleniti a kepernyon
 	public void ShowSolarStrom() {
 		gameFrame.GetFieldPanel().SetSolarstorm();
 	}
 	
+	//a parameterkent kapott aszteroida neve alapjan megkeressuk azt a drawable peldanyt a listaban amelyiknek a neve megegyezik az aszteroida nevevel, 
+	//es ennek koordinatait, és beállítjuk a parameterbeli Drawable koordinatainak
 	public void FindAsteroid(Asteroid a, Drawable d) {
 		String name = Main.getInstance().GetKey(a);
 		for(Drawable i:drawables) {
@@ -44,10 +52,12 @@ public class View {
 		}
 	}
 
+	//a parameterkent megadott drawable-t tavolitja el a listabol	
 	public void RemoveDrawable(Drawable d) {
 		drawables.remove(d);
 	}
 	
+	//a parameterkent megadott drawable-t adja hozza a listahoz
 	public void AddDrawable(Drawable d) {
 		drawables.add(d);
 	}
